e4d7c302c6eba554f72d0c9178ec3d366cbc976a
crodriguez <ccrisrober@gmail.com>
2017-05-10 12:59:57 +0200

    Added threaded tcp server version

--- clintexplorer/main.cpp
+++ clintexplorer/main.cpp
@@ -1,50 +1,105 @@
+#include <cstdlib>
 #include <iostream>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
 
-#include <manco/manco.h>
-
+using boost::asio::ip::tcp;
 namespace asio = boost::asio;
 
-std::string readline( asio::ip::tcp::socket & socket )
-{
-  asio::streambuf buf;
-  asio::read_until( socket, buf, "\n" );
+#define CLOSE_SERVER_MESSAGE "exit"
 
-  std::string data = asio::buffer_cast<const char*>(buf.data());
-  data.erase( --data.end() );  // remove the last delimeter
-  return data;
+typedef boost::shared_ptr<tcp::socket> SocketPtr;
+
+bool serverOK = true;
+
+void sendMessage( SocketPtr socket, const std::string& str )
+{
+  const std::string msg = str +"\n";
+  asio::write( *socket, asio::buffer( msg ) );
 }
 
-void sendline( asio::ip::tcp::socket & socket, const std::string& str )
+void manageMessage( SocketPtr socket, const std::string& message )
 {
-  const std::string msg = str + "\n";
-  asio::write( socket, asio::buffer(msg) );
-}
-
-int main( void )
-{
-  asio::io_service io_service;
-
-  asio::ip::tcp::acceptor acc(
-      io_service,
-      asio::ip::tcp::endpoint( asio::ip::tcp::v4(), 31400 ) );
-  asio::ip::tcp::socket socket(io_service);
-
-
-  manco::ZeqManager::init( "hbp://" );
-
-
-  std::cout << "Started TCP socket at localhost at port 31400" << std::endl;
-
-  acc.accept( socket );
-
-  while(true)
+  if ( message == "hello" )
   {
-    std::string str = readline( socket );
-    std::cout << str << std::endl;
+    sendMessage( socket, "goodbye" );
+  }
+}
 
-    sendline(socket, "pong");
-    std::cout << "sent pong" << std::endl;
+void session(SocketPtr socket)
+{
+  try
+  {
+    while( true )
+    {
+      asio::streambuf buf;
+      boost::system::error_code error;
+      asio::read_until( *socket, buf, "\n", error );
+
+      if ( error == boost::asio::error::eof )
+      {
+        break; // Connection closed cleanly by peer
+      }
+      else if ( error )
+      {
+        throw boost::system::system_error( error ); // Some other errors
+      }
+
+      std::string data = asio::buffer_cast<const char*>(buf.data());
+      data.erase( --data.end() );  // remove the last delimeter
+
+      if ( data == CLOSE_SERVER_MESSAGE )
+      {
+        serverOK = false;
+        break;
+      }
+      else
+      {
+        manageMessage( socket, data );
+      }
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception in thread: " << e.what() << "\n";
+  }
+}
+
+void server( boost::asio::io_service& io_service, short port )
+{
+  tcp::acceptor a( io_service, tcp::endpoint(tcp::v4( ), port ) );
+  while( serverOK )
+  {
+    SocketPtr sock( new tcp::socket( io_service ) );
+    a.accept( *sock );
+    auto t = boost::thread( boost::bind( session, sock ) );
+    t.join( );
+  }
+}
+
+int main( int argc, char* argv[] )
+{
+  unsigned int server_port;
+  try
+  {
+    if ( argc == 2 )
+    {
+      server_port = std::atoi( argv[ 1 ] );
+    }
+    else
+    {
+      server_port = 31400;
+    }
+
+    boost::asio::io_service io_service;
+
+    server( io_service, server_port );
+  }
+  catch ( std::exception& e )
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
   }
 
   return 0;
